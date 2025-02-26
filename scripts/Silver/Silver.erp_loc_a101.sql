2bdf08217d89da10d217080a43dc878ba44402fe
JoANeS86 <joang_86@hotmail.com>
2025-02-26 12:30:11 +0100

    Update Silver.erp_loc_a101.sql

--- scripts/Silver/Silver.erp_loc_a101.sql
+++ scripts/Silver/Silver.erp_loc_a101.sql
@@ -12,18 +12,39 @@ SELECT * FROM Bronze.erp_loc_a101
 
 -- Quality Check 1: Tweak the cid to match the format used in other tables (crm_cust_info and the transformed erp_cust_az12).
 
+INSERT INTO Silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+
 SELECT
-cid,
-CASE WHEN cid LIKE 'AW-%' THEN CONCAT(SUBSTRING(cid, 1, 2), SUBSTRING(cid, 4, LEN(cid)))
-	 ELSE cid
-END AS cid
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+	 ELSE TRIM(cntry)
+END AS cntry
 FROM Bronze.erp_loc_a101
+-- WHERE REPLACE(cid, '-', '') NOT IN (SELECT cst_key FROM Silver.crm_cust_info)
 
--- Quality Check 2: Invalid cntry values
+/* Quality Check 2: Invalid cntry values
+   After integrating the transformation in the main query above, I can add it to the Quality Check too, to compare the old
+   erroneous values to the new ones and confirm at a glance that I'm not missing anything. */
 
-SELECT DISTINCT cntry FROM Bronze.erp_loc_a101
+SELECT DISTINCT cntry
+FROM Bronze.erp_loc_a101
+ORDER BY cntry
 
 SELECT
-CASE WHEN cntry IN ('DE', 'Germany') THEN
-	 WHEN cntry IN ('US', 'USA', 'United States') THEN
-	 WHEN cntry = 'US' THEN
+	DISTINCT cntry old_cntry,
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM Bronze.erp_loc_a101
+ORDER BY cntry
+
+-- As always, final step is to apply the Quality Checks to the Silver table.
+
+SELECT * FROM Silver.erp_loc_a101
